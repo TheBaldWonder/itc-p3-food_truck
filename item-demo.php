1fa424f0355e0da700cd303140e98c700769453e
ElizaJ <eljones.millson@gmail.com>
2019-02-13 13:10:14 -0800

    Revert "Merge branch 'master' of https://github.com/TheBaldWonder/itc-p3-food_truck"
    This reverts commit 2eb8cd13c85cf36bfb122853ccb600ff075bc0fe, reversing
    changes made to ffbda0d99a05e673eb700a8fe41376bdfc857673.

--- item-demo.php
+++ item-demo.php
@@ -1,100 +1,153 @@
 <?php
-// * item-demo.php
-// * @package Taco Truck
-// * @author Elly Boyd <emorri08@seattlecentral.edu>
-// * @version 1.3 2019/02/12
-// * @link http://ellycodes.com/
-// * @license https://www.apache.org/licenses/LICENSE-2.0
+/**
+ * item-demo2.php, based on demo_postback_nohtml.php is a single page web application that allows us to request and view 
+ * a customer's name
+ *
+ * web applications.
+ *
+ * Any number of additional steps or processes can be added by adding keywords to the switch 
+ * statement and identifying a hidden form field in the previous step's form:
+ *
+ *<code>
+ * <input type="hidden" name="act" value="next" />
+ *</code>
+ * 
+ * The above live of code shows the parameter "act" being loaded with the value "next" which would be the 
+ * unique identifier for the next step of a multi-step process
+ *
+ * @package ITC281
+ * @author Bill Newman <williamnewman@gmail.com>
+ * @version 1.1 2011/10/11
+ * @link http://www.newmanix.com/
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
+ * @todo finish instruction sheet
+ * @todo add more complicated checkbox & radio button examples
+ */
 
-require 'inc_0700/config_inc.php';
+# '../' works for a sub-folder.  use './' for the root  
+require 'inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
 include 'items.php'; 
+/*
+$config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
+$config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
+$config->metaRobots = 'no index, no follow';
+$config->loadhead = ''; #load page specific JS
+$config->banner = ''; #goes inside header
+$config->copyright = ''; #goes inside footer
+$config->sidebar1 = ''; #goes inside left side of page
+$config->sidebar2 = ''; #goes inside right side of page
+$config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewable this page only)!!
+$config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
+*/
+
+//END CONFIG AREA ----------------------------------------------------------
 
 # Read the value of 'action' whether it is passed via $_POST or $_GET with $_REQUEST
 if(isset($_REQUEST['act'])){$myAction = (trim($_REQUEST['act']));}else{$myAction = "";}
 
-switch ($myAction) //check 'act' for type of process
-{ 
-    case "display": # 2)Display user's order total!
-        showData();
-        break;
-        
-    default: # 1)Ask user to place order 
-        showForm();
-} //end switch
+switch ($myAction) 
+{//check 'act' for type of process
+	case "display": # 2)Display user's order total!
+	 	showData();
+	 	break;
+	default: # 1)Ask user to place order 
+	 	showForm();
+}
 
-function showForm() # shows form so user can order from the food truck.
-{
-    global $config;
+function showForm()
+{# shows form so user can order from the food truck.
+	global $config;
     get_header(); #defaults to header_inc.php	
 	
-    echo 
-        '<script type="text/javascript" src="' . VIRTUAL_PATH . 'include/util.js"></script>
-        <script type="text/javascript">
-        function checkForm(thisForm)
-		{ //check form data for valid info
-            if(empty(thisForm.YourName,"Please Enter Your Name")){return false;}
-            return true; //if all is passed, submit!
+	echo 
+	'<script type="text/javascript" src="' . VIRTUAL_PATH . 'include/util.js"></script>
+	<script type="text/javascript">
+		function checkForm(thisForm)
+		{//check form data for valid info
+			if(empty(thisForm.YourName,"Please Enter Your Name")){return false;}
+			return true;//if all is passed, submit!
 		}
 	</script> 
 	<form action="' . THIS_PAGE . '" method="post" onsubmit="return checkForm(this);">
              ';
   
-    foreach($config->items as $item)
-    {       
-        echo '<p><strong>' . $item->Name . '</strong></p>';
-
+		foreach($config->items as $item)
+          {
+            //echo "<p>ID:$item->ID  Name:$item->Name</p>"; 
+            //echo '<p>Taco <input type="text" name="item_1" /></p>';
+              
+              echo '<p><strong>' . $item->Name . '</strong></p>';
+              echo '<p><strong>$ ' . number_format($item->Price, 2) . '</strong></p>';
             
-        echo '<p>' . $item->Description . '</p>';
             
-        echo '<p>Order Amount <input type="text" name="item_' . $item->ID . '" /></p>';
-    } //end foreach($config->items as $item)      
-    
-        echo '
-            <p>
-				<input type="submit" value="Get Order Total">
-            </p>
-            <input type="hidden" name="act" value="display" />
-        </form>
-	   ';
+                echo '<p>' . $item->Description . '</p>';
+            
+                echo '<p>Order Amount <input type="text" name="item_' . $item->ID . '" /></p>';
+          }       
+ 
+          echo '
+				<p>
+					<input type="submit" value="Get Order Total">
+				</p>
+		<input type="hidden" name="act" value="display" />
+	</form>
+	';
 	get_footer(); #defaults to footer_inc.php
-} //end showForm()
+}
 
-function showData() #form submits here we show itmes ordered
-{ 
-    get_header(); #defaults to footer_inc.php
+function showData()
+{#form submits here we show itmes ordered
+
+     get_header(); #defaults to footer_inc.php
 	
-    echo '<h3 align="center">' . smartTitle() . '</h3>';
+	echo '<h3 align="center">' . smartTitle() . '</h3>';
     
- 	foreach($_POST as $name => $value)//loop the form elements 
-    {      
-        if(substr($name,0,5)=='item_') //if form name attribute starts with 'item_', process it
-        {   //explode the string into an array on the "_"
+ 	foreach($_POST as $name => $value)
+    {//loop the form elements
+        
+        //if form name attribute starts with 'item_', process it
+        if(substr($name,0,5)=='item_')
+        {
+           
+            //explode the string into an array on the "_"
             $name_array = explode('_',$name);
 
-            //id is the second element of the array -- forcibly cast to an int in the process
+            //id is the second element of the array
+			//forcibly cast to an int in the process
             $id = (int)$name_array[1];
             
+            
             $thisItem = getItem($id);
+            //dumpDie($thisItem);
 			
+            //echo "<p>You ordered $value of item number $id</p>";
+            
             if($value!=""){
-
-                (float)$subtotal=$value*$thisItem->Price;
-                echo "<p>You ordered $value $thisItem->Name(s) which costs $" . number_format($subtotal, 2) . "</p>";
-            } //end nested if($value!="")
-        } // end if(substr($name,0,5)=='item_')   
-    } //end foreach
-
-
-    echo '<p align="center"><a href="' . THIS_PAGE . '">RESET</a></p>';
+            (float)$subtotal=$value*$thisItem->Price;
+            echo "<p>You ordered $value $thisItem->Name(s) which costs $" . number_format($subtotal, 2) . "</p>";
+            
+            }
+        }
+        
+    }
+    
+	
+	echo '<p align="center"><a href="' . THIS_PAGE . '">RESET</a></p>';
 	get_footer(); #defaults to footer_inc.php
-} //end showData
+}
+
 
 function getItem($id) {
     global $config;
     
     foreach($config->items as $item){
         if($item->ID==$id){
-            return $item; 
-        } //end if
-    } // end foreach
-} // end getItem function
+           return $item; 
+        }
+    }
+       
+}
+
+
+?>
+
--- items.php
+++ items.php
@@ -1,6 +1,6 @@
 <?php
 //items4.php
-//create items for sale in taco truck
+
 $myItem = new Item(1,"Taco","Ground beef, lettuce, and shredded cheese in a crisp corn shell!",3.50);
 $config->items[] = $myItem;
 
@@ -16,20 +16,22 @@ $config->items[] = $myItem;
 $myItem = new Item(5,"Nachos","Crispy chips, melty cheese and fresh toppings can't be beat!",7.50);
 $config->items[] = $myItem;
 
-class Item //create Item class
+class Item
 {
     public $ID = 0;
     public $Name = '';
     public $Description = '';
     public $Price = 0;
         
-    public function __construct($ID,$Name,$Description,$Price) //Item constructor
+    public function __construct($ID,$Name,$Description,$Price)
     {
         $this->ID = $ID;
         $this->Name = $Name;
         $this->Description = $Description;
-        $this->Price = $Price;    
+        $this->Price = $Price;
+        
     }#end Item constructor
+
 }#end Item class
 
 
