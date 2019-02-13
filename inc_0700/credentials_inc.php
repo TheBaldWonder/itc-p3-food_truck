25315af124f737793624dfc4dbb0e294eb37ac76
Elly <emorri08@seattlecentral.edu>
2019-02-12 22:25:06 -0800

    cleans up and formats code in item-demo and items

---
+++ inc_0700/credentials_inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * credentials_inc.php stores db connection info safe from prying eyes
+ * 
+ *  
+ * @package Survey Sez
+ * @author Elly Boyd <emorri08@seattlecentral.edu>
+ * @version 1.3 2019/02/29
+ * @link http://ellycodes.com
+ * @license https://www.apache.org/licenses/LICENSE-2.0
+ * @todo none
+ */
+
+//place credentials here:
+define('DB_NAME','itc250_wn19');
+define('DB_USER','eb_w19_admin');
+define('DB_PASSWORD','Othello5%');
+define('DB_HOST','mysql.ellycodes.com'); 
